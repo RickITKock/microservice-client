ebfaf2f0fb731bd79a46a68b2cd29f7bb4e39192
rick124 <rick@roosh.online>
2023-05-15 13:35:28 +0200

    Add create after posts/ in url

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await axios.post("http://posts.com/posts", {
+    const response = await axios.post("http://posts.com/posts/create", {
       title,
     });
 
